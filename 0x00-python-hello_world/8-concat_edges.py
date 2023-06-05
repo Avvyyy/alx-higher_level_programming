0af519d7cf42618c076d0290f61c3aea82e57fb0
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-05 13:02:46 -0700

    Reduction of file ./8-concat_edges.py to just 5 lines

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-words = str.split()
-str = ' '.join(words[5:7]) + " " + words[12] + " " + words[0]
+         language that combines remarkable power with very clear syntax"
+str = ' '.join(str.split()[5:7]) + " " + str.split()[12] + " " + str.split()[0]
 print(str)
