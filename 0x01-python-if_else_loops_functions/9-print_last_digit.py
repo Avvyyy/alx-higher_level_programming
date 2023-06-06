cdbb41cdcf71d07bced7bb7971969dcf76bbf61e
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-06 06:45:50 -0700

    Update on 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = 0
-    if number > 0:
-        last_digit = number % 10
-    elif number < 0:
+    if number < 0:
         number = abs(number)
-        last_digit = number % 10
+    last_digit = number % 10
+    print("{}".format(last_digit), end='')
     return last_digit
- 
