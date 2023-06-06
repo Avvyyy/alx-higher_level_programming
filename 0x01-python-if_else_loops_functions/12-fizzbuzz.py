78c4a92613fa739f522d6b84035d020ed44e9ee2
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-06 11:56:53 -0700

    Correction of suyntax for range() in fizzbuzz challenge and inclusion of space between print values

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1:101):
+    for i in range(1, 101):
         if i % 15 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", end=' ')
         elif i % 3  == 0:
-            print("Fizz")
+            print("Fizz", end=' ')
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz", end=' ')
         else:
-            print(i)
+            print(i, end=' ')
