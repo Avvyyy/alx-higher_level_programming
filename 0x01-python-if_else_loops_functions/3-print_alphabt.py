39c9763571d7d150b2b35c96f934505fab0f9fd4
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-06 05:15:05 -0700

    Addition of shebang to 7-islower.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,3 @@
 for letter in range(ord('a'), ord('z') + 1):
     if chr(letter) not in ['q', 'e']:
         print("{}".format(chr(letter)), end='')
-
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def is_lowercase(c):
     if ord('a') <= ord(c) <= ord('z'):
         return True
