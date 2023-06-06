ca629d931154be273210ef19db5cf05cbc47247b
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-06 12:06:21 -0700

    Creation of 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-
+    for c in str:
+        ascii_val = ord(c)
+        if 97 <= ascii_val <= 122:
+            ascii_val -= 32
+        print(chr(ascii_val), end="")
+    print()
