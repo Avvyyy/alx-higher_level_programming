5b9b8d83eb9711b2d54d748adcdda26a61145db8
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-11 14:27:34 -0700

    Creation of 12-switch.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    return [element for i, element in enumerate(my_list) if i != idx]
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return my_list
