d2bbde2d627f700f8444dda68b6a70e0fd97fadb
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-14 11:46:21 -0700

    Creation of 8-simple_delete.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_keys = sorted(a_dictionary.keys())  # Sort the keys in alphabetical order
+    sorted_keys = sorted(a_dictionary.keys())  
     for key in sorted_keys:
         print("{}: {}".format(key, a_dictionary[key]))
