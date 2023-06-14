4e8ff6be278b03f97d1a64824fcedb9ecdd9c507
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-06-14 06:56:47 -0700

    Creation of 2-uniq_add.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[number ** 2 for number in row] for row  in matrix]
+    new_matrix = [[number ** 2 for number in row] for row in matrix]
     return new_matrix
