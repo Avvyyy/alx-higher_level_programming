98857fe1d7ec64cb3c13f545c02216def102cf73
Avvvyy <uzochukwu.favour310@gmail.com>
2023-06-26 15:30:53 -0700

    Update on 3-safe_print_division.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
                 print(i, end="")
                 count += 1
         print()
-    except:
+    except IndexError:
         pass
 
     return count
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,4 @@ def safe_print_division(a, b):
         quotient = None
     finally:
         print("Inside Result: {}".format(quotient))
-
-    return quotient
+        return quotient
