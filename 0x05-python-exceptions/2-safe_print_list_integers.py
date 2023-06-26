abf97ce5bff94f123a064715a8d828733ff34a46
Avvvyy <uzochukwu.favour310@gmail.com>
2023-06-26 12:06:39 -0700

    CReation of 2-main.py and 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+            count += 1
+    except IndexError:
+        pass
+
+    print()
+    return count
