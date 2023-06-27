68895151756496f37021c08359aac5c9ba320d3a
Avvvyy <uzochukwu.favour310@gmail.com>
2023-06-27 05:10:35 -0700

    Creation of 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
