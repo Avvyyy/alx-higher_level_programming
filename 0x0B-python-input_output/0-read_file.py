3b029ebbe04988167eba8637111335ed976b9e7f
Avvvyy <uzochukwu.favour310@gmail.com>
2023-07-11 14:49:32 -0700

    First Commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,6 +11,4 @@ def read_file(filename=""):
     """
 
     with open(filename, encoding="utf-8") as txt_file:
-        file_content = txt_file.read()
-
-    print(file_content, end="")
+        print(txt_file.read(), end="")
