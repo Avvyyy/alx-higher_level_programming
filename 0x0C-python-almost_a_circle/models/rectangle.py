4f2e875ee23a0703b8c68a62626a209401c2f37f
Favour A. Uzochukwu <116796967+Avvyyy@users.noreply.github.com>
2023-07-16 08:58:54 -0700

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,8 +96,10 @@ class Rectangle(Base):
             print("")
             
     def update(self, *args, **kwargs):
-    """Update the Rectangle.
-    Args:
+        """
+        Update the Rectangle.
+        
+        Args:
         *args (ints): New attribute values.
             - 1st argument represents id attribute
             - 2nd argument represents width attribute
@@ -105,7 +107,7 @@ class Rectangle(Base):
             - 4th argument represents x attribute
             - 5th argument represents y attribute
         **kwargs (dict): New key/value pairs of attributes.
-    """
+        """
     if args and len(args) != 0:
         a = 0
         for arg in args:
