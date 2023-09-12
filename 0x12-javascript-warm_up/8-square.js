cfa4708888c5fdef593689536dab5a4846321f47
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-09-12 10:51:54 +0100

    Update of code in 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,22 +2,14 @@
 
 const args = parseInt(process.argv.slice(2)[0]);
 
-function printSquare (args) {
+function printSquare(args) {
   if (isNaN(args)) {
     console.log('Missing size');
   } else {
-    for (let i = 0; i < args; i++) {
-      let row = '';
-      for (let j = 0; j < args; j++) {
-        if (i === j || j === args - i - 1) {
-          row += 'X';
-        } else {
-          row += ' ';
-        }
-      }
-      console.log(row);
+      for (let i = 0; i < args; i++) {
+          console.log(('X'.repeat(args)));
+      }   
     }
-  }
 }
 
 printSquare(args);
