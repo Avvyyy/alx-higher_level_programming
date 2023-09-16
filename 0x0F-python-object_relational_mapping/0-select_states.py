3dde5064b7cc41282225de311441a10caed7bd31
Avvvyy <uzochukwu.favour310@gmail.com>
2023-09-16 10:49:15 -0700

    Code Update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,37 +1,53 @@
 #!/usr/bin/python3
-"""Program to retrieve states from an SQl database"""
+"""
+Program to retrieve states from a MySQL database
+"""
 
 import MySQLdb
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-database_name = sys.argv[3]
+def main():
+    """
+    Main function to retrieve and print states from the database.
+    """
+    # Check if all required command-line arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-try:
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-         host="localhost",
-         port=3306,
-         user=username,
-         passwd=password,
-         db=database_name
-    )
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+        )
 
-    # Execute the SQL query to retrieve the list of states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-    # Fetch all the results and print them
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+        # Execute the SQL query to retrieve the list of states
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-except MySQLdb.Error as e:
-    print("Error connecting to MySQL:", e)
-finally:
-    # Close the database connection
-    cursor.close()
-    db.close()
+        # Fetch all the results and print them
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+    finally:
+        # Close the database connection
+        if 'db' in locals() and db is not None:
+            cursor.close()
+            db.close()
+
+if __name__ == "__main__":
+    main()
+
