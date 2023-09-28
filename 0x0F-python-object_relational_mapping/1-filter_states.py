0b3f6f1e795709bee3101676b31b6e1cbcaf7607
Avvvyy <uzochukwu.favour310@gmail.com>
2023-09-28 12:02:13 -0700

    UPdating of permission of 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 
-
 """Lists all states with a name starting with N"""
-
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * \
-    FROM states \
-    WHERE CONVERT(`name` USING Latin1) \
-    COLLATE Latin1_General_CS \
-    LIKE 'N%';")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     states = cur.fetchall()
 
     for state in states:
-        print(state)
+        if state[1][0].upper() == 'N':
+            print(state)
