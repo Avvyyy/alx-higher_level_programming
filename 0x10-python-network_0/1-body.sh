b3f115cbcad020ea9f870ee3e601074396d532ba
Avvvyy <uzochukwu.favour310@gmail.com>
2023-09-28 12:29:22 -0700

    5-post_params.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Store the URL from the first command-line argument
-url="$1"
-
-# Use curl to send a GET request to the URL and store the response in a variable
-response=$(curl -s -w "%{http_code}" "$url")
-
-# Extract the status code from the response
-status_code=${response: -3}
-
-# Check if the status code is 200 (OK)
-if [ "$status_code" = "200" ]; then
-    # Display the body of the response
-    curl -s "$url"
-else
-    echo "Error: HTTP status code $status_code"
-fi
+#script that sends a get request and diplays the body of the response"
+curl -sL "$1"
