77003a399bd5d89cc64db646f2ba66ea23042ae5
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2023-09-29 02:32:34 -0700

    First COmmit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('utf-8'))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL."""
+
+import sys
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.headers['X-Request-Id'])
